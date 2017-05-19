b69c4e4a496d75d92fa71e275c336ee572930812
risenanti <risenanti@gmail.com>
2017-05-19 14:18:42 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # kalmanFilter
 A repo for my c++ Kalman Filter Code
+Will contain C++ code for 2 and 3d Kalman Filter and VHDL Code
---
+++ kalmanFilter.hpp
@@ -0,0 +1,17 @@
+/*Begiinning 2D Kalman Filter*/
+
+class kalmanFilter
+{
+	private:
+			/* Kalman filter variables */
+			double q; //process noise covariance
+			double r; //measurement noise covariance
+			double x; //value
+			double p; //estimation error covariance
+			double k; //kalman gain
+
+
+	public:
+
+
+};
