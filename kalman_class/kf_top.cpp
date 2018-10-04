ea77ff48683a79bed9ae436f08e6cb320c57090a
risenanti <risenanti@gmail.com>
2018-10-03 22:24:04 -0500

    updated hls files of floats

--- kalman_class/kf_top.cpp
+++ kalman_class/kf_top.cpp
@@ -46,12 +46,12 @@ kf_values filter(kf_values value){
 
 	//task3
 	//MU = M2*sinf(M0);
-	value.MU = value.M2*hls::cordic::sinf(value.M0);
+	value.MU = value.M2*sinf(value.M0);
 
 	//Task4
-	value.H0 = value.M2*hls::cordic::cosf(value.M0);
+	value.H0 = value.M2*cosf(value.M0);
 	value.H1 = 0;
-	value.H2 = hls::cordic::sinf(value.M0);
+	value.H2 = sinf(value.M0);
 
 	//Task5
 	float temp0, temp1, temp2;
@@ -62,14 +62,14 @@ kf_values filter(kf_values value){
 	value.S+=1;
 
 	//Task6
-	value.K0 = (value.P.getA1()*value.H0+value.P.getB1()*value.H1+value.P.getC1()*value.H2);
-	value.K1 = (value.P.getA2()*value.H0+value.P.getB2()*value.H1+value.P.getC2()*value.H2);
-	value.K2 = (value.P.getA3()*value.H0+value.P.getB3()*value.H1+value.P.getC3()*value.H2);
+	value.K0 = (value.P.getA1()*value.H0 + value.P.getB1()*value.H1 + value.P.getC1()*value.H2) /value.S;
+	value.K1 = (value.P.getB1()*value.H0 + value.P.getB2()*value.H1+ value.P.getB3()*value.H2)  /value.S;
+	value.K2 = (value.P.getC1()*value.H0 + value.P.getC2()*value.H1 + value.P.getC3()*value.H2) /value.S;
 
 	//task7
-	value.M0 = value.M0+value.K0*(value.Y-value.MU);
-	value.M1 = value.M0+value.K1*(value.Y-value.MU);
-	value.M2 = value.M0+value.K2*(value.Y-value.MU);
+	value.M0 = value.M0+value.K0 *(value.Y-value.MU);
+	value.M1 = value.M1+value.K1 *(value.Y-value.MU);
+	value.M2 = value.M2+value.K2 *(value.Y-value.MU);
 
 	//task8
 	temp0 = value.K0*value.S;
@@ -83,30 +83,5 @@ kf_values filter(kf_values value){
 	return value;
 }
 
-kf_values task1(kf_values value){
-	//M=A*M
-		float tempM0 = value.M0, tempM1 = value.M1, tempM2 = value.M2;
-		value.M0 = value.A.getA1()*tempM0+value.A.getA2()*tempM1 + value.A.getA3()*tempM2;
-		value.M1 = value.A.getB1()*tempM0+value.A.getB2()*tempM1 + value.A.getB3()*tempM2;
-		value.M2 = value.A.getC1()*tempM0+value.A.getC2()*tempM1 + value.A.getC3()*tempM2;
-
-		return value;
-}
-
-kf_values predict(kf_values value){
-	//M=A*M
-		float tempM0 = value.M0, tempM1 = value.M1, tempM2 = value.M2;
-		value.M0 = value.A.getA1()*tempM0+value.A.getA2()*tempM1 + value.A.getA3()*tempM2;
-		value.M1 = value.A.getB1()*tempM0+value.A.getB2()*tempM1 + value.A.getB3()*tempM2;
-		value.M2 = value.A.getC1()*tempM0+value.A.getC2()*tempM1 + value.A.getC3()*tempM2;
-
-		//task2
-		mat3 tempT2 = value.A*value.P;
-		tempT2=tempT2*value.A.transpose();
-		tempT2=tempT2+value.Q;
-		value.P=tempT2;
-
-		return value;
-}
 
 
